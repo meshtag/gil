da2730ec77084004ff684b135e25594e610e2996
meshtag <prathameshtagore@gmail.com>
2021-01-30 15:52:37 +0530

    1

--- .ci/coverage.sh
+++ .ci/coverage.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 set -e
-
 lcov --directory bin.v2 --capture --no-external --directory $(pwd) --output-file coverage.info > /dev/null 2>&1
-echo I was here
 lcov --extract coverage.info $(pwd)'/boost/gil/*' --output-file coverage.info > /dev/null
 lcov --list coverage.info
 cd libs/gil
