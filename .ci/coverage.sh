68ad87f7082f737612db4a6ea8d0a786f678c5db
meshtag <prathameshtagore@gmail.com>
2021-01-30 23:20:43 +0530

    2

--- .ci/coverage.sh
+++ .ci/coverage.sh
@@ -1,11 +1,7 @@
 #! /bin/bash
 set -e
-echo I was here 1
 lcov --directory bin.v2 --capture --no-external --directory $(pwd) --output-file coverage.info > /dev/null 2>&1
-echo 2
 lcov --extract coverage.info $(pwd)'/boost/gil/*' --output-file coverage.info > /dev/null
-echo 3
 lcov --list coverage.info
-echo 4
 cd libs/gil
 bash <(curl -s https://codecov.io/bash) -f ../../coverage.info || echo "Codecov did not collect coverage reports"
