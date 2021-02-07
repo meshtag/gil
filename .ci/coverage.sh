06b898eb9690b66d6456e4685f3bdc082e6cb41c
meshtag <prathameshtagore@gmail.com>
2021-02-07 19:24:41 +0530

    1

--- .ci/coverage.sh
+++ .ci/coverage.sh
@@ -1,20 +1,5 @@
 #! /bin/bash
-#set -e
-cd boost-root
-ls
-pwd
-cd ..
-ls
-pwd
-cd boost-root
-cd ..
-ls
-pwd
-cd boost-root
-cd ..
-ls
-pwd
-cd boost-root
+set -e
 lcov --directory bin.v2 --capture --no-external --directory $(pwd) --output-file coverage.info > /dev/null 2>&1
 lcov --extract coverage.info $(pwd)'/boost/gil/*' --output-file coverage.info > /dev/null
 lcov --list coverage.info
