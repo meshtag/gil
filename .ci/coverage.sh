2cb07d441aec5466f6e5100e124f70072ee094bb
meshtag <prathameshtagore@gmail.com>
2021-02-08 17:23:22 +0530

    4

--- .ci/coverage.sh
+++ .ci/coverage.sh
@@ -3,14 +3,14 @@ set -e
 echo "I was here in coverage.sh check error"
 cd ../boost-root
 
-./b2 -j3 coverage=on toolset=gcc variant=debug cxxstd=11 libs/gil/test/core
-./b2 -j3 coverage=on toolset=gcc variant=debug cxxstd=11 libs/gil/test/legacy
-./b2 -j3 coverage=on toolset=gcc variant=debug cxxstd=11 libs/gil/test/extension/dynamic_image
-./b2 -j3 coverage=on toolset=gcc variant=debug cxxstd=11 libs/gil/test/extension/numeric
-./b2 -j3 coverage=on toolset=gcc variant=debug cxxstd=11 libs/gil/test/extension/toolbox
-./b2 -j3 coverage=on toolset=gcc variant=debug cxxstd=11 libs/gil/test/extension/io//simple
+# ./b2 -j3 coverage=on toolset=gcc variant=debug cxxstd=11 libs/gil/test/core
+# ./b2 -j3 coverage=on toolset=gcc variant=debug cxxstd=11 libs/gil/test/legacy
+# ./b2 -j3 coverage=on toolset=gcc variant=debug cxxstd=11 libs/gil/test/extension/dynamic_image
+# ./b2 -j3 coverage=on toolset=gcc variant=debug cxxstd=11 libs/gil/test/extension/numeric
+# ./b2 -j3 coverage=on toolset=gcc variant=debug cxxstd=11 libs/gil/test/extension/toolbox
+# ./b2 -j3 coverage=on toolset=gcc variant=debug cxxstd=11 libs/gil/test/extension/io//simple
 
-# ./b2 -j3 libs/gil/test toolset=gcc cxxstd=11 variant=debug,release
+./b2 -j3 coverage=on libs/gil/test toolset=gcc cxxstd=11 variant=debug,release
 
 echo "ls bin.v2"
 lcov --directory bin.v2 --capture --no-external --directory $(pwd) --output-file coverage.info > /dev/null 2>&1
