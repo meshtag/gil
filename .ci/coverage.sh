b323ad296258be5885c3384f326e9c3d84500632
meshtag <prathameshtagore@gmail.com>
2021-02-08 15:25:17 +0530

    3

--- .ci/coverage.sh
+++ .ci/coverage.sh
@@ -2,7 +2,14 @@
 set -e
 echo "I was here in coverage.sh check error"
 cd ../boost-root
-ls bin.v2/project-cache.jam
+
+./b2 -j 2 coverage=on toolset=gcc variant=debug cxxstd=11 libs/gil/test/core
+./b2 -j 2 coverage=on toolset=gcc variant=debug cxxstd=11 libs/gil/test/legacy
+./b2 -j 2 coverage=on toolset=gcc variant=debug cxxstd=11 libs/gil/test/extension/dynamic_image
+./b2 -j 2 coverage=on toolset=gcc variant=debug cxxstd=11 libs/gil/test/extension/numeric
+./b2 -j 2 coverage=on toolset=gcc variant=debug cxxstd=11 libs/gil/test/extension/toolbox
+./b2 -j 2 coverage=on toolset=gcc variant=debug cxxstd=11 libs/gil/test/extension/io//simple
+
 echo "ls bin.v2"
 lcov --directory bin.v2 --capture --no-external --directory $(pwd) --output-file coverage.info > /dev/null 2>&1
 echo "I reached even here"
@@ -11,10 +18,3 @@ echo "I reached even even here"
 lcov --list coverage.info
 cd libs/gil
 bash <(curl -s https://codecov.io/bash) -f ../../coverage.info || echo "Codecov did not collect coverage reports"
-
-#  echo "I was even here"
-#         cd boost-root
-#         echo "I was here"
-#         export BOOST_ROOT=$(pwd)
-#         ./bootstrap.sh
-#         ls
