0ebd7f8cf9f4791403ebf51ec13f3720392c1380
meshtag <prathameshtagore@gmail.com>
2021-02-11 10:56:55 +0530

    7

--- .ci/coverage.sh
+++ .ci/coverage.sh
@@ -12,7 +12,7 @@ cd ../boost-root
 
 # echo "ls bin.v2 1"
 # ls -a bin.v2
-# ./b2 -j3 coverage=on libs/gil/test toolset=gcc cxxstd=11 variant=debug,release
+./b2 -j3 coverage=on libs/gil/test toolset=gcc cxxstd=11 variant=debug,release
 
 echo "ls bin.v2"
 ls -a bin.v2/libs/.
