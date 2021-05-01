a804a906ff174827fa72f7fdc67df535fa345c68
meshtag <prathameshtagore@gmail.com>
2021-05-01 21:21:33 +0530

    Check build1 prev

--- include/boost/gil/detail/math.hpp
+++ include/boost/gil/detail/math.hpp
@@ -313,8 +313,8 @@ inline void get_sobel_kernel(std::array<unsigned int, 2> const order,
             smoothing_kernel_size / 2 - 1, smoothing_kernel_size / 2 - 1, 3, 3));
         for (unsigned int i = 0; i < smooth_repetition; ++i)
         {
-            // unsigned int intermediate_img_size = 2 * prev_size - 1;
-            // gray32f_image_t intermediate_img(intermediate_img_size, intermediate_img_size);
+            unsigned int intermediate_img_size = 2 * prev_size - 1;
+            gray32f_image_t intermediate_img(intermediate_img_size, intermediate_img_size);
             // view_convolve(subimage_view(view(resultant_smoothing_kernel),
             //     smoothing_kernel_size / 2 - intermediate_img_size / 2,
             //     smoothing_kernel_size / 2 - intermediate_img_size / 2,
@@ -327,7 +327,7 @@ inline void get_sobel_kernel(std::array<unsigned int, 2> const order,
             //     smoothing_kernel_size / 2 - intermediate_img_size / 2,
             //     smoothing_kernel_size / 2 - intermediate_img_size / 2,
             //     intermediate_img_size, intermediate_img_size));
-            // prev_size = intermediate_img_size;
+            prev_size = intermediate_img_size;
         } 
         for (unsigned int i = 0; i < smooth_count - smooth_decrease; ++i)
         {
